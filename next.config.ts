300c7b6fe882e8c8fc4ddeb0486812d1fc9c222c
devnaveedabbasi <naveedabbasi03111309060@gmail.com>
2026-05-20 15:17:51 +0500

    add proxy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["10.112.148.30","10.136.212.30"],
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'http://98.94.99.226:5000/api/:path*', 
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
