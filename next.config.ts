afefe1a99c23b0b6bf5f767c8c20a4f879e127d9
devnaveedabbasi <naveedabbasi03111309060@gmail.com>
2026-07-21 13:53:09 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- async rewrites() {
-  return [
-    {
-      source: '/api/proxy/:path*',
-      destination: 'https://tribicx.globaleselect.it/:path*',
-    },
-  ];
-},
+//  async rewrites() {
+//   return [
+//     {
+//       source: '/api/proxy/:path*',
+//       destination: 'https://tribicx.globaleselect.it/:path*',
+//     },
+//   ];
+// },
 };
 
 module.exports = nextConfig;
